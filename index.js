16bde970eb8d727ee18e8674cbda59c67205f009
Matt <mattfields+dev@pm.me>
2024-09-11 19:59:51 +1000

    working basic rendering

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import { create } from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 const hbs = create({
   defaultLayout: 'main',
-  extname: 'hbs',
-  partialsDir: 'views',
-  layoutsDir: 'layouts',
+  extname: '.hbs',
+  partialsDir: 'views/',
+  layoutsDir: 'layouts/',
 });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
