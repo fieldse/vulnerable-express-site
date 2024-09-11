9574cee47f03df66586d2837a1853f4de54f3b1d
Matt <394065+fieldse@users.noreply.github.com>
2024-09-11 20:06:39 +1000

    Add express (#1)
    * add express + handlebars
    
    * add index.js
    
    * add layouts, views dirs
    
    * working basic rendering
    
    * add nodemon

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { create } from 'express-handlebars';
+
+const app = express();
+const hbs = create({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  partialsDir: 'views/',
+  layoutsDir: 'layouts/',
+});
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
