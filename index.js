95123518642561791e4b5abef04b703287896855
Matt <mattfields+dev@pm.me>
2024-09-12 14:16:11 +1000

    add basic pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { create } from 'express-handlebars';
+import { isLoggedIn } from './src/auth/auth-middleware.js';
 
 const app = express();
 const hbs = create({
@@ -14,8 +15,29 @@ app.set('view engine', '.hbs');
 app.set('views', './src/views');
 app.use(express.static('public'));
 
+// Home
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+// News
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+// Message board
+app.get('/message-board', (req, res) => {
+  res.render('message-board');
+});
+
+// Employee profile - PRIVATE ROUTE
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
+
+// Login
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
 app.listen(3000, () => {
