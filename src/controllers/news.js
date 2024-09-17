bb44c69dd113bd64ad4cd684c7b2f76d7d14c9c0
Matt <mattfields+dev@pm.me>
2024-09-17 23:13:08 +1000

    add rendering of news items

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,6 +1,11 @@
 // Methods for news
+import axios from 'axios';
+import { logDebug } from '../logging.js';
+import { BASE_API_URL } from '../config.js';
 
 // Render news view
 export async function newsIndex(req, res) {
-  res.render('news');
+  const { data } = await axios.get(BASE_API_URL + '/news');
+  logDebug(req, 'data', JSON.stringify(data));
+  res.render('news', { news: data?.rows });
 }
