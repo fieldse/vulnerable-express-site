22c2e641e87c549eece4812b5b640cf7238c3143
Matt <mattfields+dev@pm.me>
2024-09-18 00:57:20 +1000

    fill out admin view

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,8 +1,24 @@
 // Admin controller
-// import { logDebug } from '../logging.js';
-// import api from '../api.js';
+import { logDebug } from '../logging.js';
+import api from '../api.js';
 
 // Render news view
 export async function adminIndex(req, res) {
-  res.render('admin');
+  const messageData = await api.getMessages();
+  const newsData = await api.getNews();
+  const usersData = await api.getUsers();
+  logDebug(
+    req,
+    'admin data: ',
+    JSON.stringify({
+      messages: messageData?.data.rows,
+      news: newsData?.data.rows,
+      users: usersData?.data.rows,
+    })
+  );
+  res.render('admin', {
+    messages: messageData?.data.rows,
+    news: newsData?.data.rows,
+    users: usersData?.data.rows,
+  });
 }
