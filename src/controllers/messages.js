cfec4383d487968f2ca5870e02c7cf8a974fd536
Matt <mattfields+dev@pm.me>
2024-09-17 23:09:43 +1000

    add rendering of message data

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,6 +1,11 @@
 // Controller methods for messages
+import axios from 'axios';
+import { logDebug } from '../logging.js';
+import { BASE_API_URL } from '../config.js';
 
 // Render message board view
 export async function getMessageBoard(req, res) {
-  res.render('message-board');
+  const { data } = await axios.get(BASE_API_URL + '/messages');
+  logDebug(req, 'data', JSON.stringify(data));
+  res.render('message-board', { messages: data?.rows });
 }
